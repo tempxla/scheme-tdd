d31be38f8f09c6b39a6eec7e1803d4c6f74c210d
tempxla <38803749+tempxla@users.noreply.github.com>
2020-05-30 19:10:16 +0900

    fix test (名前衝突が起きそうなのでテストにスコープを設定)

--- money-test.scm
+++ money-test.scm
@@ -4,13 +4,15 @@
 (test-begin "money-test")
 
 ;; test-multiplication
-(define five (make-doller 5))
-(test-equal "test-multiplication" (make-doller 10) (times five 2))
-(test-equal "test-multiplication" (make-doller 15) (times five 3))
+(let ((test-name "test-multiplication"))
+  (define five (make-doller 5))
+  (test-equal test-name (make-doller 10) (times five 2))
+  (test-equal test-name (make-doller 15) (times five 3)))
 
 ;; test-equality
-(test-assert "test-equality" (equal? (make-doller 5) (make-doller 5)))
-(test-eq "test-equality" #f (equal? (make-doller 5) (make-doller 6)))
+(let ((test-name "test-equality"))
+  (test-assert test-name (equal? (make-doller 5) (make-doller 5)))
+  (test-eq test-name #f (equal? (make-doller 5) (make-doller 6))))
 
 
 (test-end "money-test")
