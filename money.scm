e195add6e6d09dd57e21f3b4de37cbb3cf4f3016
tempxla <38803749+tempxla@users.noreply.github.com>
2020-05-31 21:21:42 +0900

    fix implementation (pp.75-76)

--- money.scm
+++ money.scm
@@ -1,26 +1,15 @@
-(define-record-type <doller>
-  (make-doller _amount _currency)
-  doller?
-  (_amount doller-amount)
-  (_currency doller-currency)
+(define-record-type <money>
+  (make-money _amount _currency)
+  money?
+  (_amount money-amount)
+  (_currency money-currency)
   )
 
-(define-record-type <franc>
-  (make-franc _amount _currency)
-  franc?
-  (_amount franc-amount)
-  (_currency franc-currency)
-  )
-
-(define (money-doller x) (make-doller x "USD"))
-(define (money-franc x) (make-franc x "CHF"))
+(define (money-doller x) (make-money x "USD"))
+(define (money-franc x) (make-money x "CHF"))
 
 (define (times money x)
-  (cond
-   ((doller? money) (money-doller (* (doller-amount money) x)))
-   ((franc? money) (money-franc (* (franc-amount money) x)))))
+  (make-money (* (money-amount money) x)
+              (money-currency money)))
 
-(define (currency money)
-  (cond
-   ((doller? money) (doller-currency money))
-   ((franc? money) (franc-currency money))))
+(define currency money-currency)
